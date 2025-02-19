89d3270e80a0937c647e5388cf09e43500d83b8d
Dinah Drizella <dinahdrizella@gmail.com>
2025-02-19 16:52:31 +0300

    this is the original status of my files

---
+++ example.py
@@ -0,0 +1,18 @@
+#exampl     syntax about variable names in pyhton
+#a name given to a partition through programming is called a variable
+num1 = 65 #num1 is a variable
+num2 = 100 #num2 is a variable
+print(num1)
+print(num1 + num2)
+#the name of the variable should not start with a number or a numeral e.g 1num or 2num
+#variable name should not start with a special character eg @num, $num, !num
+#compund variable names should should not have spaces eg mun 1, num 2, my num
+#compund variable names should be joined by either an underscore or a camel case or camel number
+#aviod using capital letters to begin the variable name eg Num1, Num2
+#don't use special words as variable names eg print, input, for etc
+#variable names are names we sign to partitions on the hard disc
+#variables in pyhton are python are name sensitive eg Num1 is different from num1
+print(num2)
+#variable can be used to a new variable
+
+
---
+++ introtopython/examples.py
@@ -0,0 +1,1 @@
+#a name given to a 
