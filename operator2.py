5215fac45b97718966492ddb4186c35388a6b909
Dinah Drizella <dinahdrizella@gmail.com>
2025-02-25 10:08:18 +0300

    new files created

---
+++ operator2.py
@@ -0,0 +1,73 @@
+#comparison operators
+#these compare two values and return a boolean 
+#In mathematics we have operators like >, <,>= etc.
+
+#lets declare some variables
+comp1=20
+comp2=40
+
+print(comp1 < comp2)
+print(comp1 > comp2)
+print(comp1 >= comp2)
+print(comp1 <= comp2)
+print(comp1 != comp2)
+print(comp1 ==comp2)
+# 1. write comments on top of the line you want to comment about
+# always go to the point, follow the rules of english grammar
+# 2. ther is an assignment that is to be startesd on Thursday as shown in the google classroom.
+
+
+
+
+#Logical operators
+#we have the following, and (&), or(||), not(!)
+log1 =5
+log2 =6
+#the operator in Python returns true if both conditions are true
+print((log2 > log2) and (log2 < log1))
+
+
+print( not (log1 > log2))
+print( not (log1 < log2))
+
+print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# membership operators
+#we have in and not in 
+
+members =(20, 30, 40, 50)
+print (20 in members)
+print(20 not in members)
+
+name = "Dinah"
+
+print("n" in name)
+print("D" not in name)
+print("d" not in name)
+
+
+# identity operators
+#we use is and not is
+
+print(20 is not members)
+print(20 is 20)
+
+
